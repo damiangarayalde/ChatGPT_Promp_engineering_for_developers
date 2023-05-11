724cdb1d0d621e9ce7564a80986d128755ecc293
damiangarayalde <damiangarayalde@gmail.com>
2023-05-10 23:53:31 -0300

    updates in notebooks around bokeh and panel

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import panel as pn
-import bokeh
+
+from panel.interact import interact, fixed
+from panel import widgets
+
 pn.extension()
 
 
-pn.panel('Hello Worlds')
+def Slider(x):
+    return x*x
+
+
+interact(Slider, x=5)
